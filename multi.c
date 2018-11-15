7821e7579903f52b78caa12794fd76ae82a23e5d
dennis <dennis@localhost>
2018-11-15 10:45:15 +0800

    initial

---
+++ multi.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : multi.c
+ Author      : 
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hello this is a test"); /* prints  */
+	return EXIT_SUCCESS;
+}
